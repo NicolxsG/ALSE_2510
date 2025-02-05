13944a13fdb3f965602c135800a30bad003d3b63
Jerson Nicolás Guzmán Muñoz <jerson.guzman@mail.escuelaing.edu.co>
2025-02-05 14:01:28 -0500

    Terminado

--- eje_1/main.c
+++ eje_1/main.c
@@ -3,12 +3,16 @@
 
 
 int main(){
-  double anterior, actual;
+  double anterior, actual, constante;
   anterior = actual = 0.;
+      actual = sin(constante * i);
 
   for(int i=0; i<100; i++){
     actual = sin(M_PI * 5 / 100 * i);
-  
+    if((anterior >= 0 && actual < 0 ) || (anterior < 0 && actual >= 0))
+      printf("Hay un cruce por cero en: %d\n",i);
+    anterior = actual;constante = 1.1 * M_PI * 5 / 100;
+
   }
   return 0;
 }
